152dd0d61d9ec20fad0a79e201ab67482c64eae4
jcurbelo <robin@paella.dev>
2024-12-03 17:12:03 -0500

    adds missing polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 
 export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // Polyfill Node.js modules with browser-compatible versions
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      assert: 'assert',
+      http: 'stream-http',
+      https: 'https-browserify',
+      os: 'os-browserify/browser',
+      url: 'url',
+      buffer: 'buffer',
+      process: 'process/browser',
+    },
+  },
   optimizeDeps: {
+    include: [
+      'crypto-browserify',
+      'stream-browserify',
+      'assert',
+      'buffer',
+      'util',
+      'process',
+    ],
     esbuildOptions: {
-      // Enable esbuild polyfill plugins
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis',
+      },
+      // Remove NodeModulesPolyfillPlugin to prevent conflicts
       plugins: [
         NodeGlobalsPolyfillPlugin({
           process: true,
+          buffer: true,
         }),
-        NodeModulesPolyfillPlugin(),
       ],
     },
   },
-  plugins: [react()],
-});
+})
